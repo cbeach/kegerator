d8d106f82cd6bcf27c0e27a1d351d48e681bfb0d
Casey Beach <mcsmash@raspberrypi.(none)>
2013-11-08 00:36:48 -0800

    I did a LOT of setup.
    The code should maintain temperature now.  There are also some tools to
    use for a couple other features.

---
+++ temperature_control/firmware/.build/alamode/src/temp_control.d
@@ -0,0 +1,12 @@
+.build/alamode/src/temp_control.d .build/alamode/src/temp_control.o: .build/alamode/src/temp_control.cpp \
+ /usr/share/arduino/hardware/arduino/cores/arduino/Arduino.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/binary.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/WCharacter.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/WString.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/HardwareSerial.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/Stream.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/Print.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/Printable.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/new.h \
+ /usr/share/arduino/hardware/arduino/variants/standard/pins_arduino.h \
+ /usr/share/arduino/libraries/EEPROM/EEPROM.h
